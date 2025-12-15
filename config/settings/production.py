676c1b919e818d4eb083c7bb52a630e39ae52703
Aslhy <aslhynicol0936@gmail.com>
2025-12-15 15:03:47 -0500

    fix: default DATABASE_URL for build phase

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=config("DATABASE_URL"),
+        default=config("DATABASE_URL", default="sqlite:///db.sqlite3"),
         conn_max_age=600,
         ssl_require=True,
     )
