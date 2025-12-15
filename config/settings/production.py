31a7a5b86536e5dae47340e02a426bd5b22f8de6
Aslhy <aslhynicol0936@gmail.com>
2025-12-15 14:00:37 -0500

    Arreglos

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,9 +6,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = [
-    ".onrender.com",
-]
+ALLOWED_HOSTS = ["*"],
 
 DATABASES = {
     "default": dj_database_url.parse(
@@ -18,8 +16,33 @@ DATABASES = {
     )
 }
 
+import logging
+logging.getLogger(__name__).info("🚀 Settings PROD cargados en Koyeb")
+
+
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 APPEND_SLASH = False
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
